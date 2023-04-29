2666512eee154406fcd6cba83c33c61461429127
himel <himeeeel6@gmail.com>
2023-04-29 21:56:41 +0600

    index upload

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php include "header.php" ?>
+<?php include "navber.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style.css">
+    <title>Home-UIU E-Recieption</title>
+</head>
+<body >
+   
+<div class="container text-white" style="padding-top:70px;">
+   <div class="row">
+   <div class="col-md-12 ">
+      <div>
+      <span style="font-size:45px;">WELCOME TO </span>
+      <br>
+      <span style="font-size:70px;font-weight:bold">UNITED INTERNATIONAL UNIVERSITY</span>
+       <br>
+       <span style="font-size:50px;color:black;font-weight:bold;">e-reception</span>
+      </div>
+    </div>
+   </div>
+   <div style="margin-top:40px;"class="row">
+    <div class="col-md-4 col-12">
+      <div style="display:flex; flex-direction:column">
+              <div style="width:250px;margin-left:45px;">
+              <span >
+                      Get an appointment easily on suitable time. 
+              </span>
+              </div>
+              <div style="padding-top:20px;">
+              <a href="members.php">
+              <button class=" btn rounded-pill btn-light shadow fw-bold" style="color:#fc614f; width:80%; height:60px;  font-size:25px;"> Get Appointment</button>
+              </a>
+
+              </div>
+          </div>
+    </div>
+    <div  class="col-md-4 col-12">
+      <div style="display:flex; flex-direction:column">
+              <div style="width:250px;margin-left:45px;">
+              <span >
+                      Get clear direction of any places of our campus.
+              </span>
+              </div>
+              <div style="padding-top:20px;">
+              <a href="direction.php"><button class=" btn rounded-pill btn-light shadow fw-bold" style="color:#fc614f; width:80%; height:60px; font-size:25px;  "> Get Direction</button></a>
+              </div>
+          </div>
+    </div>
+    <div class="col-md-4 col-12">
+    <div style="display:flex; flex-direction:column">
+            <div style="width:250px;margin-left:45px;">
+            <span>
+                    Get Answer of some frequently asked questions.
+            </span>
+            </div>
+            <div style="padding-top:20px;">
+           <a href="faq.php">
+           <button class=" btn rounded-pill btn-light shadow fw-bold" style="color:#fc614f; width:80%; height:60px; font-size:25px; "> FAQ</button>
+           </a>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/632339c754f06e12d894ed7e/1gd0pjdm9';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+</body>
+</html>
