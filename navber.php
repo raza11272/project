9fec47a753c06b42d695e4a5fee815b1c91dd399
sajib <msajib191028@bscse.uiu.ac.bd>
2023-04-29 23:05:28 +0600

    uploaded

---
+++ navber.php
@@ -0,0 +1,25 @@
+
+<nav class=" navbar navbar-expand-lg navbar-dark bg-dark text-white">
+  <div class="container">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a class="nav-link active  text-white "  aria-current="page" href="index.php">Home</a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+        <a class="nav-link  text-white " style="padding-left:50px;" href="Academic_info.php">Academics</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link  text-white " style="padding-left:50px;" href="log_in.php">Log in</a>
+        </li>
+        
+      </ul>
+      <form class="d-flex" action="search.php" method="post">
+      <input name="search" class="form-control me-2 " style="border-radius:20px;" type="search" placeholder="Search" aria-label="Search" required>
+      <button type="Go" name="submit" class=" btn rounded-pill btn-dark shadow fw-bold"  style="background-image: linear-gradient(to right ,#fc604f , #f08d20);width:50px;">Go</button>
+      </form>
+    </div>
+  </div>
+</nav>
+   
